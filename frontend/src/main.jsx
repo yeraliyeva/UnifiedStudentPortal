4f1bbf51e490d5698049ee4d23eb23373d5a7b7d
Anel <a_yeraliyeva@kbtu.kz>
2026-05-11 23:09:34 +0500

    refactor(frontend): update main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { I18nProvider } from "./context/I18nContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <I18nProvider>
+      <App />
+    </I18nProvider>
   </StrictMode>
 );
